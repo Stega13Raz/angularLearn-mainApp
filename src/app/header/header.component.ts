1c43831047a56b1f052a893c071ed6761c06f871
Razvan Stegaroiu <razvanstegaroiu@gmail.com>
2017-11-17 02:04:24 +0000

    Evt binding workaround

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +7,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewEncapsulation} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -9,13 +9,20 @@ import { Recipe } from '../recipe.model';
   encapsulation: ViewEncapsulation.None
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a recipe description', 'http://i.ndtvimg.com/i/2016-07/chicken-korma_625x350_71467713811.jpg')
+    new Recipe('A test recipe', 'This is a recipe description', 'http://i.ndtvimg.com/i/2016-07/chicken-korma_625x350_71467713811.jpg'),
+    new Recipe('Another test recipe', 'This is a recipe description', 'http://i.ndtvimg.com/i/2016-07/chicken-korma_625x350_71467713811.jpg')
   ];
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
